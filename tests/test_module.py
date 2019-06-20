6568308dd2fd10a67fb3a68a8e4ef90f9e98fb01
Raymond Butcher <ray.butcher@claranet.uk>
2019-06-20 12:05:19 +0100

    Improve file management in tests

--- tests/test_module.py
+++ tests/test_module.py
@@ -1,6 +1,7 @@
 import datetime
 import os
 import time
+from pathlib import Path
 
 from pretf.test import SimpleTest
 from pretf.workflow import delete_files
@@ -17,44 +18,42 @@ class TestModule(SimpleTest):
         delete_files("src/*", "out.zip")
 
         # Create an archive.
-        with open("src/one.txt", "w") as open_file:
-            open_file.write("a")
+        Path("src/one.txt").write_text("a")
         outputs = self.apply("v1")
         assert outputs["search_results"] == ["one.txt"]
         first_output_md5 = outputs["output_md5"]
         assert os.path.exists("out.zip")
-        os.remove("out.zip")
 
-        # Add a file and affect the hash and search results.
-        with open("src/two.txt", "w") as open_file:
-            open_file.write("b")
+        delete_files("out.zip")
+
+        # Add a second file and affect the hash and search results.
+        Path("src/two.txt").write_text("b")
         outputs = self.apply("v1")
         assert outputs["search_results"] == ["one.txt", "two.txt"]
         second_output_md5 = outputs["output_md5"]
         assert first_output_md5 != second_output_md5
         assert os.path.exists("out.zip")
-        os.remove("out.zip")
+
+        delete_files("out.zip")
 
         # Change the file contents and affect the hash.
-        with open("src/two.txt", "w") as open_file:
-            open_file.write("c")
+        Path("src/two.txt").write_text("c")
         outputs = self.apply("v1")
         third_output_md5 = outputs["output_md5"]
         assert third_output_md5 != second_output_md5
         assert os.path.exists("out.zip")
-        os.remove("out.zip")
 
     def test_permissions(self):
         delete_files("src/*", "out.zip")
 
         # Create an archive.
-        with open("src/one.txt", "w") as open_file:
-            open_file.write("a")
+        Path("src/one.txt").write_text("a")
         os.chmod("src/one.txt", 0o644)
         outputs = self.apply("v1")
         first_output_md5 = outputs["output_md5"]
         assert os.path.exists("out.zip")
-        os.remove("out.zip")
+
+        delete_files("out.zip")
 
         # Change group permissions without affecting the hash.
         os.chmod("src/one.txt", 0o666)
@@ -62,7 +61,8 @@ class TestModule(SimpleTest):
         second_output_md5 = outputs["output_md5"]
         assert second_output_md5 == first_output_md5
         assert os.path.exists("out.zip")
-        os.remove("out.zip")
+
+        delete_files("out.zip")
 
         # Change executable permissions and affect the hash.
         os.chmod("src/one.txt", 0o744)
@@ -70,20 +70,19 @@ class TestModule(SimpleTest):
         third_output_md5 = outputs["output_md5"]
         assert third_output_md5 != first_output_md5
         assert os.path.exists("out.zip")
-        os.remove("out.zip")
 
     def test_timestamps(self):
         delete_files("src/*", "out.zip")
 
         # Create an archive.
-        with open("src/one.txt", "w") as open_file:
-            open_file.write("a")
+        Path("src/one.txt").write_text("a")
         when = time.mktime(datetime.datetime(2017, 7, 7, 7, 7, 7).timetuple())
         os.utime("src/one.txt", (when, when))
         outputs = self.apply("v1")
         first_output_md5 = outputs["output_md5"]
         assert os.path.exists("out.zip")
-        os.remove("out.zip")
+
+        delete_files("out.zip")
 
         # Change timestamps without affecting the hash.
         when = time.mktime(datetime.datetime(2012, 2, 2, 2, 2, 2).timetuple())
@@ -92,15 +91,13 @@ class TestModule(SimpleTest):
         third_output_md5 = outputs["output_md5"]
         assert third_output_md5 == first_output_md5
         assert os.path.exists("out.zip")
-        os.remove("out.zip")
 
     def test_disabled(self):
         delete_files("src/*", "out.zip")
 
         # Use a different configuration that has enabled = false.
         # It will not create an archive.
-        with open("src/one.txt", "w") as open_file:
-            open_file.write("a")
+        Path("src/one.txt").write_text("a")
         outputs = self.apply("v2")
         assert not outputs["output_md5"]
         assert not outputs["search_results"]
